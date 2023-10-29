30146bc9b577c955c637032bdb90adc85b3450d0
Joacim Magnusson <joacim@isogram.se>
2023-10-30 00:09:35 +0100

    Update documentation

--- README.md
+++ README.md
@@ -11,6 +11,20 @@ This plugin makes it easy to generate and modify voxel terrains in Bevy. `bevy_v
 
 ![output](https://github.com/splashdust/bevy_voxel_world/assets/428824/24a9ffd0-6b9b-40d8-aa66-c72dac575f09)
 
+The voxel terrain can be controlled in two main ways: through a terrain lookup function, and by direct `set_voxel` and `get_voxel` functions. The world has two "layers", one that is purely procedural and determined by the terrain lookup function, and one that is data based and controlled by `set_voxel`. The data layer always overrides the procedural layer. This way, the world can be infinitely large, but we only need to store information about voxel that are deliberately changed.
+
+The `set_voxel` and `get_voxel` access functions are easily reached from any normal Bevy system:
+
+```rust
+fn my_system(mut voxel_world: VoxelWorld) {
+    voxel_world.set_voxel(IVec3 { ... });
+}
+```
+
+Voxels are keyed by their XYZ coordinate in the world, specified by an `IVec3`.
+
+For an example on how to use a terrain lookup function, see [this example](https://github.com/splashdust/bevy_voxel_world/blob/main/examples/noise_terrain.rs).
+
 ## Gotchas
 
 `bevy_voxel_world` began as an internal part of a game that I'm working on, but I figured that it could be useful as a standalone plugin, for myself and perhaps for others, so I decided to break it out and make it public as a crate.
--- src/configuration.rs
+++ src/configuration.rs
@@ -15,17 +15,19 @@ pub struct VoxelWorldConfiguration {
     /// Debugging aids
     pub debug_draw_chunks: bool,
 
+    /// A function that maps voxel materials to texture coordinates.
+    /// The input is the material index, and the output is a slice of three indexes into an array texture.
+    /// The three values correspond to the top, sides and bottom of the voxel. For example,
+    /// if the slice is `[1,2,2]`, the top will use texture index 1 and the sides and bottom will use texture
+    /// index 2.
     pub texture_index_mapper: Arc<dyn Fn(u8) -> [u32; 3] + Send + Sync>,
 
     /// A function that returns a function that returns true if a voxel exists at the given position
     /// The delegate will be called every time a new chunk needs to be computed. The delegate should
     /// return a function that can be called to check if a voxel exists at a given position. This function
     /// needs to be thread-safe, since chunk computation happens on a separate thread.
-    pub voxel_lookup_delegate: Box<
-        dyn Fn(IVec3) -> Box<dyn FnMut(IVec3) -> WorldVoxel + Send + Sync>
-            + Send
-            + Sync,
-    >,
+    pub voxel_lookup_delegate:
+        Box<dyn Fn(IVec3) -> Box<dyn FnMut(IVec3) -> WorldVoxel + Send + Sync> + Send + Sync>,
 }
 
 impl Default for VoxelWorldConfiguration {
@@ -40,9 +42,7 @@ impl Default for VoxelWorldConfiguration {
                 3 => [3, 3, 3],
                 _ => [0, 0, 0],
             }),
-            voxel_lookup_delegate: Box::new(|_| {
-                Box::new(|_| WorldVoxel::Unset)
-            }),
+            voxel_lookup_delegate: Box::new(|_| Box::new(|_| WorldVoxel::Unset)),
         }
     }
 }
