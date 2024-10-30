splashdust/bevy_voxel_world

commit fd44af500c6d2f450c8cbfcb3a5cdede11ad2721

changed texture_index to use generics

examples/bombs.rs
@@ -6,6 +6,7 @@ use std::time::Duration;
 struct MainWorld;
 
 impl VoxelWorldConfig for MainWorld {
+    type Index = u8;
     fn spawning_distance(&self) -> u32 {
         15
     }

examples/multiple_worlds.rs
@@ -26,6 +26,8 @@ const BLUE: u8 = 2;
 struct MainWorld;
 
 impl VoxelWorldConfig for MainWorld {
+    type Index = u8;
+
     fn spawning_distance(&self) -> u32 {
         10
     }

examples/noise_terrain.rs
@@ -6,6 +6,8 @@ use noise::{HybridMulti, NoiseFn, Perlin};
 struct MainWorld;
 
 impl VoxelWorldConfig for MainWorld {
+    type Index = u8;
+
     fn spawning_distance(&self) -> u32 {
         25
     }

examples/custom_material.rs
@@ -23,7 +23,8 @@ const BLUE: u8 = 2;
 struct MyMainWorld;
 
 impl VoxelWorldConfig for MyMainWorld {
-    fn texture_index_mapper(&self) -> Arc<dyn Fn(u8) -> [u32; 3] + Send + Sync> {
+    type Index = u8;
+    fn texture_index_mapper(&self) -> Arc<dyn Fn(Self::Index) -> [u32; 3] + Send + Sync> {
         Arc::new(|vox_mat: u8| match vox_mat {
             RED => [1, 1, 1],
             GREEN => [2, 2, 2],

examples/fast_traversal_ray.rs
@@ -23,6 +23,8 @@ struct VoxelTrace {
 }
 
 impl VoxelWorldConfig for MyMainWorld {
+    type Index = u8;
+
     fn texture_index_mapper(&self) -> Arc<dyn Fn(u8) -> [u32; 3] + Send + Sync> {
         Arc::new(|vox_mat: u8| match vox_mat {
             SNOWY_BRICK => [0, 1, 2],

examples/ray_cast.rs
@@ -13,7 +13,9 @@ const GRASS: u8 = 2;
 struct MyMainWorld;
 
 impl VoxelWorldConfig for MyMainWorld {
-    fn texture_index_mapper(&self) -> Arc<dyn Fn(u8) -> [u32; 3] + Send + Sync> {
+    type Index = u8;
+
+    fn texture_index_mapper(&self) -> Arc<dyn Fn(Self::Index) -> [u32; 3] + Send + Sync> {
         Arc::new(|vox_mat: u8| match vox_mat {
             SNOWY_BRICK => [0, 1, 2],
             FULL_BRICK => [2, 2, 2],

examples/textures_custom_idx.rs
@@ -2,22 +2,25 @@ use bevy::prelude::*;
 use bevy_voxel_world::prelude::*;
 use std::sync::Arc;
 
-// Declare materials as consts for convenience
-const SNOWY_BRICK: u8 = 0;
-const FULL_BRICK: u8 = 1;
-const GRASS: u8 = 2;
+#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq, Default)]
+enum BlockTexture {
+    #[default]
+    SnowyBrick,
+    FullBrick,
+    Grass,
+}
 
 #[derive(Resource, Clone, Default)]
 struct MyMainWorld;
 
 impl VoxelWorldConfig for MyMainWorld {
-    type Index = u8;
+    type Index = BlockTexture;
 
     fn texture_index_mapper(&self) -> Arc<dyn Fn(Self::Index) -> [u32; 3] + Send + Sync> {
         Arc::new(|vox_mat| match vox_mat {
-            SNOWY_BRICK => [0, 1, 2],
-            FULL_BRICK => [2, 2, 2],
-            GRASS | _ => [3, 3, 3],
+            BlockTexture::SnowyBrick => [0, 1, 2],
+            BlockTexture::FullBrick => [2, 2, 2],
+            BlockTexture::Grass => [3, 3, 3],
         })
     }
 
@@ -27,6 +30,9 @@ impl VoxelWorldConfig for MyMainWorld {
 }
 
 fn main() {
+    assert_eq!(size_of::<WorldVoxel<u8>>(), 2);
+    assert_eq!(size_of::<WorldVoxel<BlockTexture>>(), 1);
+
     App::new()
         .add_plugins(DefaultPlugins)
         // We can specify a custom texture when initializing the plugin.
@@ -64,19 +70,46 @@ fn create_voxel_scene(mut voxel_world: VoxelWorld<MyMainWorld>) {
     // 20 by 20 floor
     for x in -10..10 {
         for z in -10..10 {
-            voxel_world.set_voxel(IVec3::new(x, -1, z), WorldVoxel::Solid(GRASS));
+            voxel_world.set_voxel(IVec3::new(x, -1, z), WorldVoxel::Solid(BlockTexture::Grass));
             // Grassy floor
         }
     }
 
     // Some bricks
-    voxel_world.set_voxel(IVec3::new(0, 0, 0), WorldVoxel::Solid(SNOWY_BRICK));
-    voxel_world.set_voxel(IVec3::new(1, 0, 0), WorldVoxel::Solid(SNOWY_BRICK));
-    voxel_world.set_voxel(IVec3::new(0, 0, 1), WorldVoxel::Solid(SNOWY_BRICK));
-    voxel_world.set_voxel(IVec3::new(0, 0, -1), WorldVoxel::Solid(SNOWY_BRICK));
-    voxel_world.set_voxel(IVec3::new(-1, 0, 0), WorldVoxel::Solid(FULL_BRICK));
-    voxel_world.set_voxel(IVec3::new(-2, 0, 0), WorldVoxel::Solid(FULL_BRICK));
-    voxel_world.set_voxel(IVec3::new(-1, 1, 0), WorldVoxel::Solid(SNOWY_BRICK));
-    voxel_world.set_voxel(IVec3::new(-2, 1, 0), WorldVoxel::Solid(SNOWY_BRICK));
-    voxel_world.set_voxel(IVec3::new(0, 1, 0), WorldVoxel::Solid(SNOWY_BRICK));
+    voxel_world.set_voxel(
+        IVec3::new(0, 0, 0),
+        WorldVoxel::Solid(BlockTexture::SnowyBrick),
+    );
+    voxel_world.set_voxel(
+        IVec3::new(1, 0, 0),
+        WorldVoxel::Solid(BlockTexture::SnowyBrick),
+    );
+    voxel_world.set_voxel(
+        IVec3::new(0, 0, 1),
+        WorldVoxel::Solid(BlockTexture::SnowyBrick),
+    );
+    voxel_world.set_voxel(
+        IVec3::new(0, 0, -1),
+        WorldVoxel::Solid(BlockTexture::SnowyBrick),
+    );
+    voxel_world.set_voxel(
+        IVec3::new(-1, 0, 0),
+        WorldVoxel::Solid(BlockTexture::FullBrick),
+    );
+    voxel_world.set_voxel(
+        IVec3::new(-2, 0, 0),
+        WorldVoxel::Solid(BlockTexture::FullBrick),
+    );
+    voxel_world.set_voxel(
+        IVec3::new(-1, 1, 0),
+        WorldVoxel::Solid(BlockTexture::SnowyBrick),
+    );
+    voxel_world.set_voxel(
+        IVec3::new(-2, 1, 0),
+        WorldVoxel::Solid(BlockTexture::SnowyBrick),
+    );
+    voxel_world.set_voxel(
+        IVec3::new(0, 1, 0),
+        WorldVoxel::Solid(BlockTexture::SnowyBrick),
+    );
 }

src/chunk.rs
@@ -19,17 +19,17 @@ pub(crate) const PADDED_CHUNK_SIZE: u32 = CHUNK_SIZE_U + 2;
 pub(crate) type PaddedChunkShape =
     ConstShape3u32<PADDED_CHUNK_SIZE, PADDED_CHUNK_SIZE, PADDED_CHUNK_SIZE>;
 
-pub(crate) type VoxelArray = [WorldVoxel; PaddedChunkShape::SIZE as usize];
+pub(crate) type VoxelArray<I> = [WorldVoxel<I>; PaddedChunkShape::SIZE as usize];
 
 #[derive(Component)]
 #[component(storage = "SparseSet")]
-pub(crate) struct ChunkThread<C>(pub Task<ChunkTask<C>>, PhantomData<C>);
+pub(crate) struct ChunkThread<C, I>(pub Task<ChunkTask<C, I>>, PhantomData<C>);
 
-impl<C> ChunkThread<C>
+impl<C, I> ChunkThread<C, I>
 where
     C: Send + Sync + 'static,
 {
-    pub fn new(task: Task<ChunkTask<C>>, _pos: IVec3) -> Self {
+    pub fn new(task: Task<ChunkTask<C, I>>, _pos: IVec3) -> Self {
         Self(task, PhantomData)
     }
 }
@@ -42,26 +42,26 @@ pub struct NeedsRemesh;
 pub struct NeedsDespawn;
 
 #[derive(Clone, Debug)]
-pub enum FillType {
+pub enum FillType<I> {
     Empty,
     Mixed,
-    Uniform(WorldVoxel),
+    Uniform(WorldVoxel<I>),
 }
 
 /// This is used to lookup voxel data from spawned chunks. Does not persist after
 /// the chunk is despawned.
 #[derive(Clone, Debug)]
-pub struct ChunkData {
+pub struct ChunkData<I> {
     pub position: IVec3,
-    pub voxels: Option<Arc<VoxelArray>>,
+    pub voxels: Option<Arc<VoxelArray<I>>>,
     pub voxels_hash: u64,
     pub is_full: bool,
     pub is_empty: bool,
-    pub fill_type: FillType,
+    pub fill_type: FillType<I>,
     pub entity: Entity,
 }
 
-impl ChunkData {
+impl<I: Hash + Copy> ChunkData<I> {
     pub fn new() -> Self {
         Self {
             position: IVec3::ZERO,
@@ -87,7 +87,7 @@ impl ChunkData {
         }
     }
 
-    pub fn get_voxel(&self, position: UVec3) -> WorldVoxel {
+    pub fn get_voxel(&self, position: UVec3) -> WorldVoxel<I> {
         if self.voxels.is_some() {
             self.voxels.as_ref().unwrap()[PaddedChunkShape::linearize(position.to_array()) as usize]
         } else {
@@ -123,7 +123,7 @@ impl ChunkData {
     }
 }
 
-impl Default for ChunkData {
+impl<I: Hash + Copy> Default for ChunkData<I> {
     fn default() -> Self {
         Self::new()
     }
@@ -163,16 +163,16 @@ impl<C> Chunk<C> {
 
 /// Holds all data needed to generate and mesh a chunk
 #[derive(Component)]
-pub(crate) struct ChunkTask<C> {
+pub(crate) struct ChunkTask<C, I> {
     pub position: IVec3,
-    pub chunk_data: ChunkData,
-    pub modified_voxels: ModifiedVoxels<C>,
+    pub chunk_data: ChunkData<I>,
+    pub modified_voxels: ModifiedVoxels<C, I>,
     pub mesh: Option<Mesh>,
     _marker: PhantomData<C>,
 }
 
-impl<C: Send + Sync + 'static> ChunkTask<C> {
-    pub fn new(entity: Entity, position: IVec3, modified_voxels: ModifiedVoxels<C>) -> Self {
+impl<C: Send + Sync + 'static, I: Hash + Copy + Eq> ChunkTask<C, I> {
+    pub fn new(entity: Entity, position: IVec3, modified_voxels: ModifiedVoxels<C, I>) -> Self {
         Self {
             position,
             chunk_data: ChunkData::with_entity(entity),
@@ -187,7 +187,7 @@ impl<C: Send + Sync + 'static> ChunkTask<C> {
     /// consumer.
     pub fn generate<F>(&mut self, mut voxel_data_fn: F)
     where
-        F: FnMut(IVec3) -> WorldVoxel + Send + 'static,
+        F: FnMut(IVec3) -> WorldVoxel<I> + Send + 'static,
     {
         let mut filled_count = 0;
         let modified_voxels = (*self.modified_voxels).read().unwrap();
@@ -239,7 +239,7 @@ impl<C: Send + Sync + 'static> ChunkTask<C> {
     }
 
     /// Generate a mesh for the chunk based on the currect voxel data
-    pub fn mesh(&mut self, texture_index_mapper: Arc<dyn Fn(u8) -> [u32; 3] + Send + Sync>) {
+    pub fn mesh(&mut self, texture_index_mapper: Arc<dyn Fn(I) -> [u32; 3] + Send + Sync>) {
         if self.mesh.is_none() && self.chunk_data.voxels.is_some() {
             self.mesh = Some(meshing::generate_chunk_mesh(
                 self.chunk_data.voxels.as_ref().unwrap().clone(),

commit 20884d64771c0e58dc1f2a798ef13ff0d4be5514

fix some example errors caused by fd44af50

examples/bombs.rs
@@ -11,7 +11,7 @@ impl VoxelWorldConfig for MainWorld {
         15
     }
 
-    fn voxel_lookup_delegate(&self) -> VoxelLookupDelegate {
+    fn voxel_lookup_delegate(&self) -> VoxelLookupDelegate<Self::Index> {
         Box::new(move |_chunk_pos| get_voxel_fn())
     }
 }
@@ -69,7 +69,7 @@ fn setup(mut commands: Commands) {
     });
 }
 
-fn get_voxel_fn() -> Box<dyn FnMut(IVec3) -> WorldVoxel + Send + Sync> {
+fn get_voxel_fn() -> Box<dyn FnMut(IVec3) -> WorldVoxel<u8> + Send + Sync> {
     // Set up some noise to use as the terrain height map
     let mut noise = HybridMulti::<Perlin>::new(1234);
     noise.octaves = 4;

examples/multiple_worlds.rs
@@ -32,7 +32,7 @@ impl VoxelWorldConfig for MainWorld {
         10
     }
 
-    fn voxel_lookup_delegate(&self) -> VoxelLookupDelegate {
+    fn voxel_lookup_delegate(&self) -> VoxelLookupDelegate<Self::Index> {
         Box::new(move |_chunk_pos| get_voxel_fn())
     }
 }
@@ -42,6 +42,8 @@ impl VoxelWorldConfig for MainWorld {
 struct SecondWorld;
 
 impl VoxelWorldConfig for SecondWorld {
+    type Index = u8;
+
     fn texture_index_mapper(&self) -> Arc<dyn Fn(u8) -> [u32; 3] + Send + Sync> {
         Arc::new(|vox_mat: u8| match vox_mat {
             RED => [1, 1, 1],
@@ -111,7 +113,7 @@ fn setup(mut commands: Commands, mut second_world: VoxelWorld<SecondWorld>) {
     second_world.set_voxel(IVec3::new(0, 3, 0), WorldVoxel::Solid(RED));
 }
 
-fn get_voxel_fn() -> Box<dyn FnMut(IVec3) -> WorldVoxel + Send + Sync> {
+fn get_voxel_fn() -> Box<dyn FnMut(IVec3) -> WorldVoxel<u8> + Send + Sync> {
     // Set up some noise to use as the terrain height map
     let mut noise = HybridMulti::<Perlin>::new(1234);
     noise.octaves = 4;

examples/noise_terrain.rs
@@ -12,7 +12,7 @@ impl VoxelWorldConfig for MainWorld {
         25
     }
 
-    fn voxel_lookup_delegate(&self) -> VoxelLookupDelegate {
+    fn voxel_lookup_delegate(&self) -> VoxelLookupDelegate<Self::Index> {
         Box::new(move |_chunk_pos| get_voxel_fn())
     }
 }
@@ -58,7 +58,7 @@ fn setup(mut commands: Commands) {
     });
 }
 
-fn get_voxel_fn() -> Box<dyn FnMut(IVec3) -> WorldVoxel + Send + Sync> {
+fn get_voxel_fn() -> Box<dyn FnMut(IVec3) -> WorldVoxel<u8> + Send + Sync> {
     // Set up some noise to use as the terrain height map
     let mut noise = HybridMulti::<Perlin>::new(1234);
     noise.octaves = 5;